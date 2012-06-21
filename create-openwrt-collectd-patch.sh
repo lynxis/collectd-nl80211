4fec5577c6c87c11dffa6d0d703abb6a4436aa98
Alexander Couzens <lynxis@c-base.org>
2012-06-21 02:05:08 +0200

    fixed creating patch

--- create-openwrt-collectd-patch.sh
+++ create-openwrt-collectd-patch.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 cat build_system.patch > 999-nl80211.patch
-diff -urp /dev/null collectd-nl80211.c >> 999-nl80211.patch
+cat >> 999-nl80211.patch  <<EOF
+diff --git a/src/collectd-nl80211.c b/src/collectd-nl80211.c
+new file mode 100644
+index 0000000..366124a
+EOF
+diff -u /dev/null  collectd-nl80211.c   | tail -n+3 >> 999-nl80211.patch
